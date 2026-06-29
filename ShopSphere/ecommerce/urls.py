74e34513e0d34237ee2a3710375a6c79b9eb563f
Iliyaz <jan03iliyaz@gmail.com>
2026-06-30 01:44:20 +0530

    Completed Lesson 8 - Dynamic Products

--- ShopSphere/ecommerce/urls.py
+++ ShopSphere/ecommerce/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from accounts.views import home
 
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', home, name='home'),
+    path("admin/", admin.site.urls),
+    path("", home, name="home"),
+    path("products/", include("products.urls")),
 ]
 
 if settings.DEBUG:
